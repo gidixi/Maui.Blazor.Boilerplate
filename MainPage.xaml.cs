1d6fdd12b777660bebc36c55e091c69aca18515b
gidixi <g.desolda@gmail.com>
2024-09-09 21:27:14 +0200

    Aggiungere i file di progetto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.Blazor.Boilerplate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
